c91d117a5a54bbe3d620363337db7ac9628c2a5e
Shayne <shayneheminger@live.com>
2016-09-30 22:53:14 -0400

    True Initial Commit

--- VideoHomeStorageTIB/VHSEncoder.cs
+++ VideoHomeStorageTIB/VHSEncoder.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace VideoHomeStorageTIB
+namespace VideoHomeStorage.TIB
 {
-    public class Class1
+    public class VHSEncoder
     {
     }
 }
