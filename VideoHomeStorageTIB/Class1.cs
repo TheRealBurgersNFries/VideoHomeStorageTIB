a55d17548d6f4a9233515c29562a3da865c570b5
Shayne <Shayne@SORBITAL-SURF>
2016-09-30 22:51:31 -0400

    Add project files.

---
+++ VideoHomeStorageTIB/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoHomeStorageTIB
+{
+    public class Class1
+    {
+    }
+}
